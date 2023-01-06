59c5cc242798965a071dfbd22dacf02154525312
redgum <redgum775@gmail.com>
2023-01-06 21:51:00 +0900

    Update: 設定メニューから使用モデル変更と、字幕の位置を変更する機能を追加

--- src/app.py
+++ src/app.py
@@ -7,8 +7,10 @@ from transcription import Transcription
 class Application:
   def __init__(self, args):
     self.model = args.model
+    self.model_list = ['tiny','base', 'small', 'medium', 'large']
     self.font_size = args.font_size
     self.side = args.side
+    self.side_list = ['top', 'bottom']
 
     # tkinterでWindowを構築
     root = self.builder_window()
@@ -29,6 +31,35 @@ class Application:
     # 白を透過する
     root.wm_attributes('-transparentcolor', 'white')
 
+    # メニューバー
+    menubar = tk.Menu(root, bg='green', fg='blue')
+    # 設定
+    config_menu = tk.Menu(menubar, tearoff=False)
+    # モデル選択
+    config_model_menu = tk.Menu(config_menu, tearoff=False)
+    self.model_var = tk.IntVar()
+    config_model_menu.add_radiobutton(label='tiny', command=self.config_model_menu_click, variable=self.model_var, value=0)
+    config_model_menu.add_radiobutton(label='base', command=self.config_model_menu_click, variable=self.model_var, value=1)
+    config_model_menu.add_radiobutton(label='small', command=self.config_model_menu_click, variable=self.model_var, value=2)
+    config_model_menu.add_radiobutton(label='medium', command=self.config_model_menu_click, variable=self.model_var, value=3)
+    config_model_menu.add_radiobutton(label='large', command=self.config_model_menu_click, variable=self.model_var, value=4)
+    self.model_var.set(self.model_list.index(self.model))
+    # フォントサイズ
+    config_font_size_menu = tk.Menu(config_menu, tearoff=False)
+    # 字幕の位置
+    config_side_menu = tk.Menu(config_menu, tearoff=False)
+    self.side_var = tk.IntVar()
+    config_side_menu.add_radiobutton(label='top', command=self.config_side_menu_click, variable=self.side_var, value=0)
+    config_side_menu.add_radiobutton(label='bottom', command=self.config_side_menu_click, variable=self.side_var, value=1)
+    self.side_var.set(self.side_list.index(self.side))
+
+    # サブメニューを配置
+    config_menu.add_cascade(label='モデル', menu=config_model_menu)
+    config_menu.add_cascade(label='フォントサイズ', menu=config_font_size_menu)
+    config_menu.add_cascade(label='字幕の位置', menu=config_side_menu)
+    menubar.add_cascade(label="設定", menu=config_menu)
+    root.config(menu=menubar)
+
     # ボールド体、フォントサイズ30に設定
     myfont = font.Font(weight='bold', size=self.font_size)
 
@@ -39,6 +70,28 @@ class Application:
       self.label.pack(side=tk.TOP)
 
     return root
+  
+  def config_model_menu_click(self):
+    with open('src/config.json', 'r') as json_file:
+      config_dict = json.load(json_file)
+      self.model = self.model_list[self.model_var.get()]
+      config_dict['user_config']['model'] = self.model
+    with open('src/config.json', 'w') as json_file:
+      json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
+  
+  def config_side_menu_click(self):
+    with open('src/config.json', 'r') as json_file:
+      config_dict = json.load(json_file)
+      self.side = self.side_list[self.side_var.get()]
+      config_dict['user_config']['side'] = self.side
+    with open('src/config.json', 'w') as json_file:
+      json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
+
+    if self.side == 'bottom':
+      self.label.pack(side=tk.BOTTOM)
+    elif self.side == 'top':
+      self.label.pack(side=tk.TOP)
+
 
 def get_args():
   parser = argparse.ArgumentParser()

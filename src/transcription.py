f860bab9a9256d14a70a22d86cbdb0fb96f5158e
redgum <redgum775@gmail.com>
2023-01-10 18:30:34 +0900

    Update: 音声認識モデル切り替え処理を追加

--- src/transcription.py
+++ src/transcription.py
@@ -4,38 +4,71 @@ import soundcard as sc
 import threading
 import queue
 import numpy as np
-import argparse
 
 class Transcription:
-  def __init__(self, model='base', label=None):
+  def __init__(self, model_name='base', label=None):
     self.SAMPLE_RATE = 16000
     self.INTERVAL = 3
     self.BUFFER_SIZE = 4096
 
     self.label = label
+    self.model_name = model_name
+    self.model = None
+    self.load_model()
 
-    # whisperから自動音声認識モデルを読み込み
-    print('音声認識モデルを読み込み中・・・')
-    self.model = whisper.load_model(model, device='cpu')
-    if torch.cuda.is_available():
-      _ = self.model.cuda()
-      print('GPU推論モードで実行します')
-    else:
-      print('CPU推論モードで実行します')
-    print('-------------------------')
+    self.th_recording = None
+    self.th_recognize = None
+
+    self.recognize_flg = False
 
     self.recognition_waiting_audio = queue.Queue()
     self.b = np.ones(100) / 100
 
     self.options = whisper.DecodingOptions(language='ja', fp16=False)
+  
+  def load_model(self):
+    # whisperから自動音声認識モデルを読み込み
+    print(f'音声認識モデル({self.model_name})を読み込み中・・・')
+    self.model = whisper.load_model(self.model_name, device='cpu')
+    if torch.cuda.is_available():
+      print(f'音声認識モデル({self.model_name})をGPU上に配置中・・・')
+      _ = self.model.cuda()
+      print('GPU推論モードで実行します')
+    else:
+      print('CPU推論モードで実行します')
+    print('-------------------------')
+  
+  def switch_model(self, model_name):
+    # 使用モデルを切り替える処理のスレッドを立ち上げ
+    th_switch_model = threading.Thread(target=self._switch_model, args=(model_name,), daemon=True)
+    th_switch_model.start()
+
+  def _switch_model(self, model_name):
+    # 使用モデルを切り替える処理
+    self.recognize_flg = False
+    self.th_recognize.join()
+    del self.model
+    self.model_name = model_name
+    self.load_model()
+    self.recognize_flg = True
+    self.start_recognize()
 
   def start_transcription(self):
     # システム音声の自動認識を開始
-    self.th_recognize = threading.Thread(target=self.recognize, daemon=True)
+    self.recognize_flg = True
+    self.start_rocording()
+    self.start_recognize()
+  
+  def start_rocording(self):
+    # システム音声の収録スレッドを開始
     self.th_recording = threading.Thread(target=self.recording, daemon=True)
-    self.th_recognize.start()
     self.th_recording.start()
 
+  def start_recognize(self):
+    # 音声認識スレッドを開始
+    self.th_recognize = threading.Thread(target=self.recognize, daemon=True)
+    self.th_recognize.start()
+  
   def recording(self):
     # システム音声を取得
     with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=self.SAMPLE_RATE, channels=1) as mic:
@@ -62,6 +95,8 @@ class Transcription:
   def recognize(self):
     # 音声認識
     while True:
+      if not self.recognize_flg:
+        break
       audio = self.recognition_waiting_audio.get()
       if(audio ** 2).max() > 0.001:
         audio = whisper.pad_or_trim(audio)
@@ -69,6 +104,10 @@ class Transcription:
         mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
         result = whisper.decode(self.model, mel, self.options)
 
-        print(f'recognize result: {result.text}')
+        print(f'({self.model_name}) recognize result: {result.text}')
         if self.label is not None:
           self.label['text']=result.text
+    # 停止処理
+    print('-------------------------')
+    print(f'音声認識モデル({self.model_name})は停止されました')
+    self.label['text']='認識モデル切り替え中です・・・'

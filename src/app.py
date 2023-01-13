2b955f9dfbbc40da3fd799aa0ebdb36ba3e0e741
redgum <redgum775@gmail.com>
2023-01-13 16:22:39 +0900

    Refactor: ウィンドウ構築に関するコードを修正

--- src/app.py
+++ src/app.py
@@ -22,9 +22,10 @@ class Application:
 
   def builder_window(self):
     root = tk.Tk()
-    root.title('Overlay-Auto-Subtitle')
+    root.title('Live Caption using Whisper')
     root.geometry('500x300')
     root.config(bg='green')
+
     # 常に最前面に配置
     root.wm_attributes('-topmost', True)
     # 緑を透過する
@@ -32,9 +33,11 @@ class Application:
 
     # メニューバー
     menubar = tk.Menu(root)
-    # 設定
+
+    # 設定メニュー
     config_menu = tk.Menu(menubar, tearoff=False)
-    # モデル選択
+
+    # モデル選択選択メニュー
     config_model_menu = tk.Menu(config_menu, tearoff=False)
     self.model_var = tk.StringVar()
     config_model_menu.add_radiobutton(label='tiny', command=self.config_model_menu_click, variable=self.model_var, value='tiny')
@@ -43,14 +46,16 @@ class Application:
     config_model_menu.add_radiobutton(label='medium', command=self.config_model_menu_click, variable=self.model_var, value='medium')
     config_model_menu.add_radiobutton(label='large', command=self.config_model_menu_click, variable=self.model_var, value='large')
     self.model_var.set(self.model)
-    # 使用言語
+
+    # 使用言語選択メニュー
     config_lang_menu = tk.Menu(config_menu, tearoff=False)
     self.lang_var = tk.StringVar()
     config_lang_menu.add_radiobutton(label='日本語', command=self.config_lang_menu_click, variable=self.lang_var, value='ja')
     config_lang_menu.add_radiobutton(label='English', command=self.config_lang_menu_click, variable=self.lang_var, value='en')
     config_lang_menu.add_radiobutton(label='Auto', command=self.config_lang_menu_click, variable=self.lang_var, value='auto')
     self.lang_var.set(self.lang)
-    # フォントサイズ
+
+    # フォントサイズ選択メニュー
     config_font_size_menu = tk.Menu(config_menu, tearoff=False)
     self.font_size_var = tk.IntVar()
     config_font_size_menu.add_radiobutton(label='16', command=self.config_font_size_menu_click, variable=self.font_size_var, value=16)
@@ -58,14 +63,15 @@ class Application:
     config_font_size_menu.add_radiobutton(label='32', command=self.config_font_size_menu_click, variable=self.font_size_var, value=32)
     config_font_size_menu.add_radiobutton(label='40', command=self.config_font_size_menu_click, variable=self.font_size_var, value=40)
     self.font_size_var.set(self.font_size)
-    # 字幕の位置
+
+    # 字幕の位置選択メニュー
     config_side_menu = tk.Menu(config_menu, tearoff=False)
     self.side_var = tk.StringVar()
     config_side_menu.add_radiobutton(label='top', command=self.config_side_menu_click, variable=self.side_var, value='top')
     config_side_menu.add_radiobutton(label='bottom', command=self.config_side_menu_click, variable=self.side_var, value='bottom')
     self.side_var.set(self.side)
 
-    # サブメニューを配置
+    # サブメニューを配置、メニューバーに設定メニューを配置
     config_menu.add_cascade(label='モデル', menu=config_model_menu)
     config_menu.add_cascade(label='言語', menu=config_lang_menu)
     config_menu.add_cascade(label='フォントサイズ', menu=config_font_size_menu)
@@ -74,61 +80,81 @@ class Application:
     menubar.add_cascade(label="設定", menu=config_menu)
     root.config(menu=menubar)
 
-    # フォントを設定
-    myfont = font.Font(weight='bold', size=self.font_size)
+    # 字幕ラベルを配置
+    self.label = tk.Label(root, text='ここに字幕が表示されます')
+    self.set_subtitle_font()
+    self.set_subtitle_position()
 
-    self.label = tk.Label(root, text='ここに字幕が表示されます', font=myfont)
+    return root
+
+  # 字幕のフォントを設定する
+  def set_subtitle_font(self):
+    myfont = font.Font(weight='bold', size=self.font_size)
+    self.label.config(font=myfont)
+  
+  # 字幕の表示位置を設定する
+  def set_subtitle_position(self):
     if self.side == 'bottom':
       self.label.pack(side=tk.BOTTOM)
     elif self.side == 'top':
       self.label.pack(side=tk.TOP)
-    return root
   
+  # モデル選択選択メニューからモデルが選択されたときの処理
   def config_model_menu_click(self):
+    # configファイルに反映
     with open('src/config.json', 'r') as json_file:
       config_dict = json.load(json_file)
       self.model = self.model_var.get()
       config_dict['user_config']['model'] = self.model
     with open('src/config.json', 'w') as json_file:
       json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
-    # モデルを切り替える
+
+    # モデルを切り替える処理
     self.ts.switch_model(self.model)
   
+  # 使用言語選択メニューから言語が選択されたときの処理
   def config_lang_menu_click(self):
+    # configファイルに反映
     with open('src/config.json', 'r') as json_file:
       config_dict = json.load(json_file)
       self.lang = self.lang_var.get()
       config_dict['user_config']['lang'] = self.lang
     with open('src/config.json', 'w') as json_file:
       json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
+
+    # 音声認識の言語を切り替える処理
     self.ts.set_decoding_option(lang=self.lang)
 
+  # フォントサイズ選択メニューからフォントサイズが選択されたときの処理
   def config_font_size_menu_click(self):
+    # configファイルに反映
     with open('src/config.json', 'r') as json_file:
       config_dict = json.load(json_file)
       self.font_size = self.font_size_var.get()
       config_dict['user_config']['font-size'] = self.font_size
     with open('src/config.json', 'w') as json_file:
       json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
-    # フォントサイズを切り替える
-    myfont = font.Font(weight='bold', size=self.font_size)
-    self.label.config(font=myfont)
+    
+    # 字幕のフォントを変える処理
+    self.set_subtitle_font()
   
+  # 字幕の位置選択メニューから位置が選択されたときの処理
   def config_side_menu_click(self):
+    # configファイルに反映
     with open('src/config.json', 'r') as json_file:
       config_dict = json.load(json_file)
       self.side = self.side_var.get()
       config_dict['user_config']['side'] = self.side
     with open('src/config.json', 'w') as json_file:
       json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
-
-    if self.side == 'bottom':
-      self.label.pack(side=tk.BOTTOM)
-    elif self.side == 'top':
-      self.label.pack(side=tk.TOP)
+    
+    # 字幕の位置を変える処理
+    self.set_subtitle_position()
   
+  # 設定メニューからデフォルトに戻すが選択されたときの処理
   def set_to_default(self):
-    with open('src/config.json', 'r') as json_file:
+    # configファイルに反映
+    with open('src/config.json', 'w+') as json_file:
       config_dict = json.load(json_file)
       config_dict['user_config'] = config_dict['default_config']
       self.model = config_dict['user_config']['model']
@@ -139,10 +165,11 @@ class Application:
     with open('src/config.json', 'w') as json_file:
       json.dump(config_dict, json_file, ensure_ascii=False, indent=2, separators=(',', ': '))
     
-    if self.side == 'bottom':
-      self.label.pack(side=tk.BOTTOM)
-    elif self.side == 'top':
-      self.label.pack(side=tk.TOP)
+    # デフォルトに戻す処理
+    self.ts.switch_model(self.model)
+    self.ts.set_decoding_option(lang=self.lang)
+    self.set_subtitle_font()
+    self.set_subtitle_position()
 
 def get_args():
   parser = argparse.ArgumentParser()
